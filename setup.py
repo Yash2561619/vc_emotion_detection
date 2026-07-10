5a33646cea824f7237b2b16b9c916355b67669b8
Yash Kabure <yashkabure@gmail.com>
2026-07-10 23:06:43 +0530

    Migrated project to clean local directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='detect emtion from text',
+    author='Yash',
+    license='',
+)
